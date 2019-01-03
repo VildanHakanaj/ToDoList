04ce8aa57b6fb65a66eb14d718b33af176aad8d9
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2019-01-03 16:19:37 -0500

    initial commit with the design already coded

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $removeModal = $('#removeModal');
+    $closeBtn = $('.close-btn');
+    $removeBtn = $('.remove');
+    $noBtn = $('#removeModal a:last-of-type');
+    $addBtn = $('.add');
+    $createItemModal = $('#createItemModal');
+
+    //Open the modal for removing items
+    $removeBtn.on('click', function () { 
+        $removeModal.addClass('show-me');
+    });
+
+
+    //Open the modal for adding items
+    $addBtn.on('click', function(){
+        $createItemModal.addClass('show-me');
+    });
+
+
+    $noBtn.on('click', function(){
+        $removeModal.removeClass('show-me');
+    });
+    
+    //Close the modal when the x is clicked
+    $closeBtn.on('click', function(){
+        $removeModal.removeClass('show-me');
+        $createItemModal.removeClass('show-me');
+    });
+});
+
